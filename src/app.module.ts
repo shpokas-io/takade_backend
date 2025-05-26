ccade790475a75be48af521631b0bc61bd3cd3db
Skirmantas Špakovskis <s.shpokas@gmail.com>
2025-05-26 19:30:25 +0300

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
 import { SupabaseModule } from './supabase.module';
 
 @Module({
   imports: [CoursesModule, SupabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
